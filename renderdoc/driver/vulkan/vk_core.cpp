c5a63d0694c38ec841a3000c351aefb65900d62f
artyomd <artyom.dangizyan@gmail.com>
2024-09-26 20:24:10 -0400

    fix vulkan layer loading for macos
    use the same VK_LAYER_EXPORT as in linux and android as the vulkan loader couldn't find the symbols in the layer, add VK_KHR_PORTABILITY_ENUMERATION extension to the supported extensions as the vulkan driver on macOS is not fully  conformant Vulkan implementation. Add the flags parameter in VkInitParams as VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR should be used in case if VK_KHR_PORTABILITY_ENUMERATION extension is being enabled.

--- renderdoc/driver/vulkan/vk_core.cpp
+++ renderdoc/driver/vulkan/vk_core.cpp
@@ -49,7 +49,7 @@ RDOC_DEBUG_CONFIG(bool, Vulkan_Debug_SingleSubmitFlushing, false,
 uint64_t VkInitParams::GetSerialiseSize()
 {
   // misc bytes and fixed integer members
-  size_t ret = 128;
+  size_t ret = 160;
 
   ret += AppName.size() + EngineName.size();
 
@@ -99,6 +99,8 @@ void VkInitParams::Set(const VkInstanceCreateInfo *pCreateInfo, ResourceId inst)
 
   for(uint32_t i = 0; i < pCreateInfo->enabledExtensionCount; i++)
     Extensions[i] = pCreateInfo->ppEnabledExtensionNames[i];
+
+  flags = pCreateInfo->flags;
 
   InstanceID = inst;
 }
@@ -1633,6 +1635,10 @@ static const VkExtensionProperties supportedExtensions[] = {
     {
         VK_KHR_PIPELINE_LIBRARY_EXTENSION_NAME,
         VK_KHR_PIPELINE_LIBRARY_SPEC_VERSION,
+    },
+    {
+        VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME,
+        VK_KHR_PORTABILITY_ENUMERATION_SPEC_VERSION,
     },
     {
         VK_KHR_PRESENT_ID_EXTENSION_NAME,
